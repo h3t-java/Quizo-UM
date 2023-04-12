21a07767135fd5fabbe5c7bec552a59405fbeabf
h3t-java <ivan.ivanov.h3t@gmail.com>
2023-04-12 15:20:31 +0300

    First commit

---
+++ src/main/java/com/project/QuizoUM/QuizoUmApplication.java
@@ -0,0 +1,13 @@
+package com.project.QuizoUM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizoUmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizoUmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/QuizoUM/QuizoUmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.QuizoUM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizoUmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
